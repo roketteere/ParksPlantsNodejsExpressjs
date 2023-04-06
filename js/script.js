73571c5dbad49e4910d3ab168496c9b3fa1a2a05
Willlasbury <asburyw229@gmail.com>
2023-04-06 15:22:39 -0700

    test

--- js/script.js
+++ js/script.js
@@ -2,4 +2,4 @@
 // use google maps to query the location and return a latitude and longitude
 // take the latitude and longitude from google and give it to breezometer for list
     // of allergens in that area
-// return
+// return allergen information to user
