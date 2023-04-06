b59c11ca63d1dacd55046e90e2b71e5fa2981511
Willlasbury <asburyw229@gmail.com>
2023-04-06 15:17:36 -0700

    add boiler plate code

---
+++ js/script.js
@@ -0,0 +1,5 @@
+// take a location and plant of interest string as a user input
+// use google maps to query the location and return a latitude and longitude
+// take the latitude and longitude from google and give it to breezometer for list
+    // of allergens in that area
+// return
