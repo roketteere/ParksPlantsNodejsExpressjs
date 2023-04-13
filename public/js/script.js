00b77dfab87d4cdb26e01a758ad8ca32dd68fe53
Erick-McGee <erickmcgee3@gmail.com>
2023-04-13 10:15:36 -0700

    yeehaw

--- public/js/script.js
+++ public/js/script.js
@@ -1,16 +1,80 @@
 // import trefle fetch
-var trefleData = [];
+// var trefleData = [];
 // get json
 
-var data = fetch('json/data.json').then(response => response.json()).then(data => { // ds = JSON.stringify(data);
-    console.log(data.data[0]);
+// var data = fetch('json/data.json').then(response => response.json()).then(data => { // ds = JSON.stringify(data);
+//     console.log(data.data[0]);
 
-});
+// });
 
+let banana = [{
+    id: 109482,
+    common_name: 'Common nettle',
+    slug: 'urtica-dioica',
+    scientific_name: 'Urtica dioica',
+    year: 1753,
+    bibliography: 'Sp. Pl.: 984 (1753)',
+    author: 'L.',
+    status: 'accepted',
+    rank: 'species',
+    family_common_name: null,
+    genus_id: 5550,
+    image_url: 'https://bs.plantnet.org/image/o/9db58cbb3538a6b77384f972971d51869228e545',
+    synonyms: [Array],
+    genus: 'Urtica',
+    family: 'Urticaceae',
+    links: [Object]
+  },
+  {
+    id: 227114,
+    common_name: 'Barnyard grass',
+    slug: 'dactylis-glomerata',
+    scientific_name: 'Dactylis glomerata',
+    year: 1753,
+    bibliography: 'Sp. Pl.: 71 (1753)',
+    author: 'L.',
+    status: 'accepted',
+    rank: 'species',
+    family_common_name: null,
+    genus_id: 10915,
+    image_url: 'https://bs.plantnet.org/image/o/f84a7d4fc2e627ccd451f568479b1932c2b2d900',
+    synonyms: [Array],
+    genus: 'Dactylis',
+    family: 'Poaceae',
+    links: [Object]
+  },]
+
+
+
+  class Plant{
+    constructor(commonName, scientificName, plantImage, year, genus, family, synonyms){
+        this.commonName=commonName
+        this.scientificName=scientificName;
+        this.plantImage = plantImage
+        this.year = year
+        this.genus = genus
+        this.family = family
+        this.synonyms = synonyms
+    }
+}
+function sortTrefleAreaSearch(array){
+    let plantArray = []
+    for (let i = 0; i < array.length; i++) {
+        let commonName = array[i].common_name;
+        let scientificName = array[i].scientific_name
+        let plantImage = array[i].image_url
+        let year = array[i].year
+        let genus = array[i].genus
+        let family = array[i].family
+        let synonyms = array[i].synonyms
+        let plant = new Plant(commonName, scientificName, plantImage, year, genus, family, synonyms)
+        plantArray.push(plant)
+    }
+return plantArray}
 
 // Save search history to local storage
 var search_history = [];
-var plants = [];
+// var plants = [];
 
 
 // Element Variables
@@ -26,25 +90,40 @@ function findPlants() {
     searchContainer.classList.add('hide')
     resultsPage.classList.remove('hide')
     modalSearchButton.classList.remove('hide')
+    searchModal.classList.add('hide')
 }
 
+let plantBanana = sortTrefleAreaSearch(banana)
 // create and append plant information
-function displayPlantInfo() {
-    plants.forEach(function (plants) {
+function displayPlantInfo(data) {
+    for (let i = 0; i < data.length; i++) {
+        console.log(i)
         var plantCard = document.createElement("div")
         plantCard.classList.add("col", "s6", "m4", "xl2", "plant-card");
         var card = document.createElement("div")
         card.classList.add("card", "hoverable", "rounded")
         var cardImage = document.createElement("div")
-        cardImage.classList.add("card-image")
+        cardImage.classList.add("card-image", "overflow", "plant-image")
         var plantImage = document.createElement("img")
-        plantImage.setAttribute("src", plants[i].imageUrl)
+        plantImage.classList.add("plant-image")
         var cardContent = document.createElement("div")
         cardContent.classList.add("card-content")
-        var cardTitle = document.createElement("span")
-        cardTitle.classList.add("card-title", truncate)
-        var scientificName = document.createElement("p")
-        scientificName.classList.add("scientific-name", "truncate")
+        var cardTitle = document.createElement("span") 
+        cardTitle.classList.add("card-title", "truncate")
+        var scienceName = document.createElement("p")
+        scienceName.classList.add("scientific-name", "truncate")
+
+        // hidden
+        var plantYear = document.createElement('p')
+        plantYear.classList.add('hide')
+        var plantGenus = document.createElement('p')
+        plantGenus.classList.add('hide')
+        var plantFamily = document.createElement('p')
+        plantFamily = document.createElement('hide')
+        var plantSynonyms = document.createElement('p')
+        plantSynonyms.classList.add('hide')
+
+
         // append data
         resultsPage.appendChild(plantCard)
         plantCard.appendChild(card)
@@ -52,32 +131,45 @@ function displayPlantInfo() {
         cardImage.appendChild(plantImage)
         card.appendChild(cardContent)
         cardContent.appendChild(cardTitle)
-        cardContent.appendChild(scientificName)
-    })
-}
+        cardContent.appendChild(scienceName)
 
+        // fill each section with plant data
+        plantImage.setAttribute("src", data[i].plantImage)
+        cardTitle.textContent = data[i].commonName
+        scienceName.textContent = data[i].scientificName
+    }
+}
+displayPlantInfo(plantBanana)
 // modal elements
 
-// plant info modal
-var modalButton = document.getElementById("modal-button")
-var closeButton = document.querySelector("#close-button")
-var modal = document.querySelector(".qmodal")
-var searchModal = document.querySelector("#search-modal")
+    // plant info modal
+    var modalButton = document.getElementById("modal-button")
+    var closeButton = document.querySelector("#close-button")
+    var modal = document.querySelector(".qmodal")
+    var searchModal = document.querySelector("#search-modal")
 
-// search modal
-var modalSearchButton = document.querySelector(".modal-search-button")
+    // search modal
+    var modalSearchButton = document.querySelector(".modal-search-button")
 
 
-// function openSearch
-function openSearch() {
-    searchModal.classList.remove("hide")
-}
-function closeModal() {
-    modal.classList.add("hide")
-}
+// modal functions
 
-modalSearchButton.addEventListener("click", openSearch)
-closeButton.addEventListener("click", closeModal)
+    // open search modal
+    function openSearch() {
+        searchModal.classList.remove("hide")
+    }
+
+    // close current modal
+    function closeModal() {
+        modal.classList.add("hide")
+    }
+
+        // event listeners
+        modalSearchButton.addEventListener("click", openSearch)
+        closeButton.addEventListener("click", closeModal)
+
+
+
 
 
 // save search to history
